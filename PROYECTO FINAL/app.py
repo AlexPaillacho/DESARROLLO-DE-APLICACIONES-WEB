08e9c87433a547059313c9b4d643e83d3ac2bd13
AlexPaillacho <ad.natop@uea.edu.ec>
2026-02-22 11:12:23 -0500

    cambios

--- PROYECTO FINAL/app.py
+++ PROYECTO FINAL/app.py
@@ -1,9 +1,25 @@
-from flask import flask
-app = flask(__name__)
-@pp.router("/")
-def hello_Wordl():
-    return "hello, Wordl"
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "hello, World- Bienvenidos!"
+
+
+@app.route('/Page1/<StandPage>')
+def Page1(StandPage):
+    return f'Saludos Bienvenido nesecitas una, {StandPage}!' 
+
+
+@app.route('/Page2/<LandPage>')
+def Page2(LandPage):
+    return f'Bienvenido nesecitas una, {LandPage}!'
+
+@app.route('/Page3/<CorporacPage>')
+def Page3(CorporacPage):
+    return f'Bienvenido a la pagina para tu empresa , {CorporacPage}!'
+    
 
 if __name__ == "__main__":
     app.run(debug=True)
-    
