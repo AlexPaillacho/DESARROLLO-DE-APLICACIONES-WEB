0899d9a65effc9be1d1450e9c52e555d929be854
AlexPaillacho <ad.natop@uea.edu.ec>
2026-03-14 16:46:36 -0500

    cambios

--- PROYECTO FINAL/app.py
+++ PROYECTO FINAL/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, url_for, request, redirect, flash
+
 from form import productoForm
  
 app = Flask(__name__)
@@ -22,6 +23,19 @@ def Page3(CorporacPage):
     return f'Bienvenido a la pagina para tu empresa, {CorporacPage}!'
 
 #ruta de productos
+@app.route('/productos/nuevo', methods =['GET', 'POST'])
+def producto_nuevo():
+    form=productoForm()
+    if form.validate_on_submit():
+        nombre = form.nombre.data
+        descripcion = form.descripcion.data
+        cantidad = form.cantidad.data
+        precio = form.cantidad.data
+        
+        flash(' Producto agregado exitosamente')
+
+        return redirect(url_for("inico"))
+    return render_template("producto_form.html", form=form)
 
 
 if __name__ == "__main__":
